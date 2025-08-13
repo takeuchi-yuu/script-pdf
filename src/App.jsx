cc72db3060ae717e28d22e8c2a8986c13fda06aa
takeuchi-yuu <zhuneikaifayong@gmail.com>
2025-08-14 03:10:19 +0900

    TailwindCSSが効くようにした

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
-export default App = () => {
+export const App = () => {
   // TODO: Appコンポーネントを実装
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <p className="text-red-500">test</p>
+    </div>
+  );
 };
